98bf5446f4669d68f5b4b8581e0e185852f5f6db
nagarjun-chodapaneedi <nagarjun.chodapaneedi@freshworks.com>
2020-09-06 10:32:35 +0530

    added seed phrase toggle screens

--- TrustlessCapital/src/screens/seedphrase.js
+++ TrustlessCapital/src/screens/seedphrase.js
@@ -9,14 +9,15 @@ import {
 } from 'react-native';
 import styles from '../stylesheets/seedphrase';
 import WalletServices from '../services/wallet-services';
-import SortableGrid from 'react-native-sort-grid';
+import SortableGrid from '../components/sortable-grid';
 
 export default class SeedPhraseScreen extends Component {
   state = {
     saveButtonText: 'Save',
     resetButtonText: 'Reset',
     seedPhrase: [],
-    isVerificationMode: false
+    isVerificationMode: false,
+    seedPhraseText: ''
   };
 
   constructor(props) {
@@ -25,22 +26,56 @@ export default class SeedPhraseScreen extends Component {
   }
 
   _generateMnemonic() {
-    let seedPhrase = WalletServices.createMnemonic().split(' ');
+    let seedPhraseText = WalletServices.createMnemonic();
+    let seedPhrase = seedPhraseText.split(' ');
     this.state.seedPhrase = seedPhrase;
-    this.setState({seedPhrase});
+    this.state.seedPhraseText = seedPhraseText;
+    this.setState({seedPhrase: seedPhrase, seedPhraseText: seedPhraseText});
   }
 
-  _getPhraseItem(phrase, index) {
-    return (
-      <>
-        <View style={styles.phraseItem}>
-          {index != null ? (
-             <View style={styles.phraseItemWrapper}><Text style={styles.phraseIndex}>{index + 1}</Text></View>
+  _onSaveButtonClick() {
+    if (this.state.isVerificationMode) {
+
+    } else {
+      this.setState({isVerificationMode : true});
+    }
+  }
+
+  _onResetButtonClick() {
+    if (this.state.isVerificationMode) {
+      this.setState({isVerificationMode: false});
+      this._generateMnemonic();
+    } else {
+      this._generateMnemonic();
+    }
+  }
+
+  _getSortableGrid() {
+    return <SortableGrid
+      style={styles.phrasesWrapper}
+      itemWidth={Dimensions.get('window').width / 2}
+      itemHeight={Dimensions.get('window').height / 12}
+      itemsPerRow={2}
+      onDragRelease={itemOrder =>
+        console.log(
+          'Drag was released, the blocks are in the following order: ',
+          itemOrder,
+        )
+      }>
+      {this.state.seedPhrase.map((phrase, index) => (
+        <View
+          style={styles.phraseItem}
+          key={index}
+          inactive={!this.state.isVerificationMode}>
+          {!this.state.isVerificationMode ? (
+            <View style={styles.phraseItemWrapper}>
+              <Text style={styles.phraseIndex}>{index + 1}</Text>
+            </View>
           ) : null}
           <Text style={styles.phraseText}>{phrase}</Text>
         </View>
-      </>
-    );
+      ))}
+    </SortableGrid>;
   }
 
   render() {
@@ -48,45 +83,24 @@ export default class SeedPhraseScreen extends Component {
       <SafeAreaView style={styles.wrapper}>
         <KeyboardAvoidingView style={{flex: 1}}>
           <View style={styles.container}>
-            <Text style={styles.mainTitle}>Mnemonic Seed Phrase</Text>
+            <Text style={styles.mainTitle}>{this.state.isVerificationMode? 'Verify ' : ''}Mnemonic Seed Phrase</Text>
             <View style={styles.header}>
               <Text style={styles.title}>Disclaimer</Text>
               <Text style={styles.subTitle}>
-                Please note down your 12 word mnemonic seed phrase in the
-                displayed order and save it in a secure manner.
+                {this.state.seedPhraseText}
               </Text>
             </View>
-            <SortableGrid
-              style={styles.phrasesWrapper}
-              itemWidth={Dimensions.get('window').width / 2}
-              itemHeight={Dimensions.get('window').height / 12}
-              itemsPerRow={2}
-              onDragRelease={itemOrder =>
-                console.log(
-                  'Drag was released, the blocks are in the following order: ',
-                  itemOrder,
-                )
-              }>
-              {this.state.seedPhrase.map((phrase, index) => (
-                <View style={styles.phraseItem} key={index}>
-                  {!this.state.isVerificationMode ? (
-                    <View style={styles.phraseItemWrapper}>
-                      <Text style={styles.phraseIndex}>{index + 1}</Text>
-                    </View>
-                  ) : null}
-                  <Text style={styles.phraseText}>{phrase}</Text>
-                </View>
-              ))}
-            </SortableGrid>
+            {this._getSortableGrid()}
             <View style={styles.footer}>
-              <TouchableOpacity style={styles.primaryButtonStyle}>
+              <TouchableOpacity style={styles.primaryButtonStyle}
+              onPress={this._onSaveButtonClick.bind(this)}>
                 <Text style={styles.primaryButtonText}>
                   {this.state.saveButtonText}
                 </Text>
               </TouchableOpacity>
               <TouchableOpacity
                 style={styles.secondaryButtonStyle}
-                onPress={this._generateMnemonic.bind(this)}>
+                onPress={this._onResetButtonClick.bind(this)}>
                 <Text style={styles.secondaryButtonText}>
                   {this.state.resetButtonText}
                 </Text>
